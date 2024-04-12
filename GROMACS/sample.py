1b1673390dd991f8c2e1f2a012446b4caea67a6d
QFlachaire <69212724+QFlachaire@users.noreply.github.com>
2024-04-12 16:45:22 +0200

    Create sample.py

---
+++ GROMACS/sample.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import qarnot
+
+# Create a connection
+conn = qarnot.Connection('qarnot.conf')
+
+# Create a task
+task = conn.create_task('md-gromacs', 'docker-batch', 1)
+
+# Create a resource bucket and add iput files 
+input_bucket = conn.create_bucket('md-gromacs-input')
+input_bucket.sync_directory('input/')
+
+# Attach the bucket to the task
+task.resources.append(input_bucket)
+
+# Create a result bucket and attach it to the task
+output_bucket = conn.create_bucket('md-gromacs-output')
+task.results = output_bucket
+
+# Set the command to run when launching the container, by overriding a constant.
+# Task constants are the main way of controlling a task's behaviour
+task.constants['DOCKER_REPO'] = 'qarnotlab/gromacs'
+task.constants['DOCKER_TAG'] = '2021.2'
+# task.constants['DOCKER_CMD'] = "./run_md.sh"
+task.constants['DOCKER_CMD'] = 'sh -c "./run_md.sh 2>&1 | tee output.log"'
+
+# Submit the task to the Api, that will launch it on the cluster
+task.submit()
