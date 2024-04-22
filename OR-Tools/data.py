2de79e8ec269c7ed681f02d833f23618ae517a8b
Anselme <anselme.donato@qarnot-computing.com>
2024-04-22 16:17:33 +0200

    adding OR-Tools

---
+++ OR-Tools/data.py
@@ -0,0 +1,23 @@
+def create_data() :
+    
+    num_nurses = 5
+    num_shifts = 3
+    num_days = 7
+    all_nurses = range(num_nurses)
+    all_shifts = range(num_shifts)
+    all_days = range(num_days)
+    # Each element of the triple is 0 or 1, indicating whether a shift was requested.
+    # For example, the triple [0, 0, 1] in the fifth position of row 1 indicates that 
+    # nurse 1 requests shift 3 on day 5.
+    shift_requests = [[[0, 0, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 1],
+                       [0, 1, 0], [0, 0, 1]],
+                      [[0, 0, 0], [0, 0, 0], [0, 1, 0], [0, 1, 0], [1, 0, 0],
+                       [0, 0, 0], [0, 0, 1]],
+                      [[0, 1, 0], [0, 1, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0],
+                       [0, 1, 0], [0, 0, 0]],
+                      [[0, 0, 1], [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0],
+                       [1, 0, 0], [0, 0, 0]],
+                      [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 0, 0], [1, 0, 0],
+                       [0, 1, 0], [0, 0, 0]]]
+    
+    return num_nurses, num_shifts, num_days, all_nurses, all_shifts, all_days, shift_requests
---
+++ OR-Tools/sample.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import sys
+import qarnot
+
+# Create a connection
+conn = qarnot.Connection(client_token='<<<MY_SECRET_TOKEN>>>')
+
+# Create a task
+task = conn.create_task('ortools-demo', 'docker-batch', 1)
+
+# Store if an error happened
+error_happened = False
+try:
+	# Create a resource bucket and add iput files 
+	input_bucket = conn.create_bucket('ortools-demo-input')
+	input_bucket.sync_directory('or-tools-examples/input_CP/')
+
+	# Attach the bucket to the task
+	task.resources.append(input_bucket)
+
+	# Create a result bucket and attach it to the task
+	output_bucket = conn.create_bucket('ortools-demo-output')
+	task.results = output_bucket
+	task.results_blacklist = "__pycache__"
+
+	# Set the command to run when launching the container, by overriding a constant.
+	# Task constants are the main way of controlling a task's behaviour
+	task.constants['DOCKER_REPO'] = 'qarnotlab/ortools'
+	task.constants['DOCKER_TAG'] = '9.5'
+	task.constants['DOCKER_CMD'] = "python3 run_optimization.py"
+
+	# Submit the task to the API, that will launch it on the cluster
+	task.submit()
+
+	# Wait for the task to be finished, and monitor the progress of its deployment
+	last_state = ''
+	done = False
+	while not done:
+		if task.state != last_state:
+			last_state = task.state
+			print("** {}".format(last_state))
+
+		# Wait for the task to complete, with a timeout of 5 seconds.
+		# This will return True as soon as the task is complete, or False
+		# after the timeout.
+		done = task.wait(5)
+
+		# Display fresh stdout / stderr
+		sys.stdout.write(task.fresh_stdout())
+		sys.stderr.write(task.fresh_stderr())
+
+	# Display errors on failure
+	if task.state == 'Failure':
+		print("** Errors: %s" % task.errors[0])
+		error_happened = True
+	else:
+		task.download_results('output')
+
+finally:
+	# Exit code in case of error
+	if error_happened:
+		sys.exit(1)
