52644e054df798a198be73f1334d04cde0b4eb9a
cecilebetmont <164189686+cecilebetmont@users.noreply.github.com>
2024-05-03 14:47:17 +0200

    Create run.py for openfoam foundation

---
+++ openfoam-foundation/run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Import the Qarnot SDK
+import qarnot
+import os
+
+# Create a connection, from which all other objects will be derived
+# Enter client token here
+
+conn = qarnot.connection.Connection(client_token='<<<TOKEN_ID>>>')
+
+# -------------------------------------------------------------------------- #
+NB_NODES = 2
+# -------------------------------------------------------------------------- #
+
+# Create a task
+task = conn.create_task("OpenFOAM Tests",
+                        "openfoam-foundation", NB_NODES)
+
+
+# Create the input bucket and synchronize with a local folder
+# Insert a local folder directory
+input_bucket = conn.create_bucket("openfoam-in")
+input_bucket.sync_directory("input")
+
+# Attach the bucket to the task
+task.resources.append(input_bucket)
+
+# Create a result bucket and attach it to the task
+task.results = conn.create_bucket("openfoam-out")
+
+task.constants['OPENFOAM_INPUT_DIRECTORY_NAME'] = 'motorbike-'+os.getenv("DOCKER_TAG")
+task.constants['RUN_SCRIPT'] = "Allrun"
+task.constants['DOCKER_TAG'] = os.getenv("DOCKER_TAG")
+
+# Define checkpoint
+task.snapshot(60)
+
+# Submit the task
+task.submit()
