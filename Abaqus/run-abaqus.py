021492fc0763524d7091486b53ab8d50d804ae80
thais-farias <167104588+thais-farias@users.noreply.github.com>
2024-06-05 11:54:10 +0200

    RUn updated with less parameters

--- Abaqus/run-abaqus.py
+++ Abaqus/run-abaqus.py
@@ -3,24 +3,17 @@
 # Import the Qarnot SDK
 import qarnot
 
-# Variables
-task_name = 'Hello World Abaqus'
-
+# Connect to the Qarnot platform
 conn = qarnot.Connection(client_token='<<<MY_SECRET_TOKEN>>>')
+
+# Create a task
 task = conn.create_task('Hello world Abaqus', "abaqus", 1)
 
 # Create input and output buckets
 input_bucket = conn.create_bucket("abaqus-in")
-input_bucket.sync_directory('input/')
+input_bucket.sync_directory('taylor3d')
 task.resources.append(input_bucket)
 task.results = conn.create_bucket('abaqus-out')
-
-# Abaqus-specific commands for RoCE
-run_computation='mpirun --allow-run-as-root --bind-to none --hostfile /job/mpihosts -x UCX_NET_DEVICES=mlx5_2:1 -x UCX_IB_TRAFFIC_CLASS=124 -x UCX_IB_SL=3 /root/abaqus'
-
-# Run computation's input bucket file 
-task.constants['DOCKER_CMD_MASTER'] = f'/bin/bash -c "/job/utils/setup_cluster abaqus job=elementradial.inp"'
-task.constants['DOCKER_CMD_WORKER'] = '/bin/bash -c "/job/utils/setup_cluster"'
 
 
 # Submit the task and download results
