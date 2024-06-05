320ca2c629061e559ea362550303519527a1f449
thais-farias <167104588+thais-farias@users.noreply.github.com>
2024-06-05 10:58:49 +0200

    deleted license variables and standardized output bucket name

--- Abaqus/run-abaqus.py
+++ Abaqus/run-abaqus.py
@@ -8,21 +8,6 @@ task_name = 'Hello World Abaqus'
 
 conn = qarnot.Connection(client_token='<<<MY_SECRET_TOKEN>>>')
 task = conn.create_task('Hello world Abaqus', "abaqus", 1)
-# Setting up task constants
-
-task.constants['ABAQUS_LICENSE_IP'] = '<<<YOUR_LICENSE_IP>>>'
-task.constants['ABAQUS_LICENSE_PORT'] = '<<<YOUR_LICENSE_PORT>>>'
-task.constants['ABAQUS_VENDOR_PORT'] = '<<<YOUR_VENDOR_PORT>>>'
-
-# Modify the custom_v6.env file
-env_file_path = '/usr/SIMULIA/EstProducts/2021/linux_a64/SMA/site/custom_v6.env'
-with open(env_file_path, 'r') as file:
-    env_content = file.read()
-
-env_content = env_content.replace('ABAQUS_LICENSE_PORT', task.constants.ABAQUS_LICENSE_PORT).replace('ABAQUS_LICENSE_IP', task.constants.ABAQUS_LICENSE_IP)
-
-with open(env_file_path, 'w') as file:
-    file.write(env_content)
 
 # Create input and output buckets
 input_bucket = conn.create_bucket("abaqus-in")
@@ -39,4 +24,4 @@ task.constants['DOCKER_CMD_WORKER'] = '/bin/bash -c "/job/utils/setup_cluster"'
 
 
 # Submit the task and download results
-task.run(output_dir="abaqus-results")
+task.run(output_dir="abaqus-out")
